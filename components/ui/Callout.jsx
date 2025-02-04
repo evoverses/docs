3c8a42ab202e9b800f5cf4b86637d70a04cb65be
DanM <83934559+danmancs@users.noreply.github.com>
2025-02-04 15:45:54 +1000

    Created first pages

---
+++ components/ui/Callout.jsx
@@ -0,0 +1,7 @@
+export function Callout({ children }) {
+    return (
+        <div className="border-l-4 border-green-500 bg-green-100 p-4 rounded-md">
+            { children }
+        </div>
+    );
+}
